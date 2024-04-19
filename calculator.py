836a5c7169475c5fba7d9924e12719f3112d2e2a
CayoH10 <cayoh000@gmail.com>
2024-04-19 20:53:22 -0300

    Vala minha nossa senhora

---
+++ calculator.py
@@ -0,0 +1,16 @@
+def soma(a, b):
+    return a + b
+
+resultado = soma(2, 4) & subtracao(4, 5)
+print (resultado)
+
+def subtracao(a, b):
+    return a - b
+
+def multiplicacao(a, b):
+    return a * b 
+
+def divisao(a, b):
+    return a / b
+
+
